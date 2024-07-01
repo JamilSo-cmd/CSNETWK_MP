2db39eed34f40f9aa67e13b446d2c495d4b8d080
JamilSo-cmd <brandon_jamil_so@dlsu.edu.ph>
2024-07-01 19:22:48 +0800

    set up client switch cases

--- Client.java
+++ Client.java
@@ -4,10 +4,52 @@ import java.util.*;
 
 public class Client {
     
+    Socket clientEndpoint;
+    //driver for Client
+    //run java Client <username>
     public static void main(String args[]){
 
-		String sServerAddress = args[0];
-		int nPort = Integer.parseInt(args[1]);
+		String sServerAddress;
+		String username = args[0];
+        String input; 
+        int nPort;
+        
+        boolean flag = true;
+        Scanner scanner = new Scanner(System.in);
+
+
+        while(flag == true){
+            System.out.println("Hello " + username +", Enter Command:");
+            input = scanner.next();
+            
+            switch (input){
+
+                case "/join": 
+                sServerAddress = scanner.next();
+                nPort = scanner.nextInt();
+                System.out.println("input is "+sServerAddress+" and " + nPort);
+                connectToServer(sServerAddress,nPort);
+                break;
+                case "/leave":;
+                break; 
+                case "/register":;
+                break;
+                case "/store":;
+                break;
+                case "/dir":;
+                break;
+                case "/get":;
+                break;
+                case "/close": flag = false;
+                break;
+                default: System.out.println("try again");
+            }
+        }
+
+    }
+    
+    //connects client to server
+    public static void connectToServer(String sServerAddress, int nPort){
         
         try{
 
@@ -15,14 +57,9 @@ public class Client {
 
 			System.out.println("Client: Connected to server at" + clientEndpoint.getRemoteSocketAddress());
             
-			clientEndpoint.close();
         }
         catch(Exception e){
             e.printStackTrace();
         }
-        finally{
-			System.out.println("Client: Connection is terminated.");
-        }
     }
-
 }
